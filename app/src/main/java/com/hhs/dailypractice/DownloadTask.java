073d4cd2a3f401902dcc204e25c60d7f14b54310
Hanhesong <hesong.han@gmail.com>
2020-04-24 22:58:39 +0800

    Handler and AsyncTask

---
+++ app/src/main/java/com/hhs/dailypractice/DownloadTask.java
@@ -0,0 +1,30 @@
+package com.hhs.dailypractice;
+
+import android.os.AsyncTask;
+
+public class DownloadTask extends AsyncTask<Void,Integer,Boolean> {
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        super.onPostExecute(aBoolean);
+    }
+
+
+
+}
