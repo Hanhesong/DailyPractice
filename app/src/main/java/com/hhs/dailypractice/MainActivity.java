168861ac1c3903f742b07386da6a7263e816bc58
Hanhesong <hesong.han@gmail.com>
2020-06-14 22:23:14 +0800

    add menu and intent

--- app/src/main/java/com/hhs/dailypractice/MainActivity.java
+++ app/src/main/java/com/hhs/dailypractice/MainActivity.java
@@ -1,13 +1,17 @@
 package com.hhs.dailypractice;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +30,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnChange.setOnClickListener(this);
         btnNext.setOnClickListener(this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 
     private Handler handler = new Handler() {
@@ -56,7 +81,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }).start();
                 break;
             case R.id.btnNext:
-                startActivity(new Intent(this, FileStorageActivity.class));
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
                 break;
 
             default:
